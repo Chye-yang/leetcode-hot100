b61ccf5100947e850571731ed4b57000580b8c45
Chye-yang <chye.y000@gmail.com>
2025-02-24 13:07:36 +0800

    844

--- ycy/数组/删除元素/844. 比较含退格的字符串.cpp
+++ ycy/数组/删除元素/844. 比较含退格的字符串.cpp
@@ -1,19 +1,66 @@
-#include <iostream>
-#include <string>
-#include <vector>
+class Solution
+{
+public:
+    bool backspaceCompare(string s, string t)
+    {
+        int fast = 0, slow = 0;
 
-using namespace std;
-
-
-class Solution {
-    public:
-        bool backspaceCompare(string s, string t) {
-            s=='#'
-            int fast=0,slow=0;
-            for(fast=0;fast<s.length;fast++){
-                
-                if(s[])
-
+        // 处理字符串 s
+        for (fast = 0; fast < s.length(); fast++)
+        {
+            if (s[fast] != '#')
+            {
+                s[slow] = s[fast]; // 保存有效字符
+                slow++;
+            }
+            else
+            {
+                if (slow > 0)
+                {
+                    slow--; // 退格，回退一个字符
+                }
             }
         }
-    };
+
+        // 记录 s 处理后的有效长度
+        int s_len = slow;
+
+        // 重置 slow，准备处理 t
+        slow = 0;
+        for (fast = 0; fast < t.length(); fast++)
+        {
+            if (t[fast] != '#')
+            {
+                t[slow] = t[fast]; // 保存有效字符
+                slow++;
+            }
+            else
+            {
+                if (slow > 0)
+                {
+                    slow--; // 退格，回退一个字符
+                }
+            }
+        }
+
+        // 记录 t 处理后的有效长度
+        int t_len = slow;
+
+        // 如果处理后的有效字符数不同，直接返回 false
+        if (s_len != t_len)
+        {
+            return false;
+        }
+
+        // 比较处理后的有效字符
+        for (int i = 0; i < s_len; i++)
+        {
+            if (s[i] != t[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};

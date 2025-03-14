4c51e2c1f934066ee8d6780315a181ed815d21af
Chye-yang <chye.y000@gmail.com>
2025-03-14 18:08:15 +0800

    ii

--- ycy/栈和队列/单调栈/496. 下一个更大元素 I.cpp
+++ ycy/栈和队列/单调栈/496. 下一个更大元素 I.cpp
@@ -6,19 +6,25 @@
 
 using namespace std;
 
-class Solution {
-    public:
-        vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-            vector<int> res(nums1.size(),-1);
-            for(int i=0;i<nums1.size();i++){
-                if(auto it=nums2.find(nums1[i])!=nums2.end()){
-                    for(int j=it+1;j<nums2.size();j++){
-                        if(nums2[j]>nums1[i]){
-                            res[i]=j;
-                        }
+class Solution
+{
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+    {
+        vector<int> res(nums1.size(), -1);
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            auto it = find(nums2.begin(), nums2.end(), nums1[i])
+            {
+                for (auto j = it + 1; j < nums2.end(); j++)
+                {
+                    if (*j > nums1[i])
+                    {
+                        res[i] = *j;
                     }
                 }
             }
-            return res;
         }
-    };
+        return res;
+    }
+};
